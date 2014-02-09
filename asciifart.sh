635b22684784e75cc697c2ba23101db8376f3553
blucoat <jameshagborg@gmail.com>
2014-02-08 20:07:34 -0500

    Create asciifart.sh

---
+++ asciifart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+url="http://www.asciiartfarts.com/index.html" 
+filter="/<pre>/,/<\/pre>/ {if (\$0 ~ /<pre>/ || \$0 ~ /<\/pre>/) {} else {print;}}"
+
+while getopts nr opt; do 
+  case $opt in 
+    n) filter="/<font [^>]*><pre>/,/<\/pre>/ {if (\$0 ~ /<font [^>]*><pre>/ || \$0 ~ /<\/pre>/) {} else {print;}}" ;;
+    r) url="http://www.asciiartfarts.com/random.cgi" ;; 
+    *) echo "unrecognised option -$opt" 
+       exit ;;
+  esac 
+done 
+
+wget "$url" -O - -q | awk -- "$filter"
